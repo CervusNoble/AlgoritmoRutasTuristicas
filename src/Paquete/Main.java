7c7506dbef45b71ef949a8b7ee9e49ee448664ca
noble620 <guillerojas2008@hotmail.com>
2020-07-17 22:11:12 -0500

    Optimal path design

--- src/Paquete/Main.java
+++ src/Paquete/Main.java
@@ -1,5 +1,6 @@
 package Paquete;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -24,33 +25,61 @@ public class Main {
             poi_conectados.add(poi_c);
         }
 
-        formar_posi(new ArrayList<Integer>(),0,0);            //Se enviar el POI de inicio (siempre empieza por 0) a poi_anterior.
-
-        for(Ruta iterador:lista_rutas){
-            System.out.println(iterador.getRuta()+" Valor: "+iterador.getPuntaje());                  //Imprime las rutas validas/optimas con su valor.
-        }
+        formar_posi(new ArrayList<Integer>(),new ArrayList<String>(), 0, "0" );            //Se enviar el POI de inicio (siempre empieza por 0) a poi_anterior.
+        mostrar_rutas();
+        rutas_aptas();
     }
 
-    public static void formar_posi(ArrayList<Integer> recorrido, int poi_anterior,double valor){
+    public static void formar_posi(ArrayList<Integer> recorrido, ArrayList<String> valor, int poi_anterior, String val_anterior){
         recorrido.add(poi_anterior);                            //Se agrega el POI siguiente al recorrido.
-
+        valor.add(val_anterior);
         if(recorrido.size()==n_poi){
+            BigDecimal res = new BigDecimal("0");
             Ruta r = new Ruta();
-            Iterator<Integer> iterador = recorrido.iterator();
-            while (iterador.hasNext()){
-                r.setRuta(poi[iterador.next()]);                //Guarda las rutas validas en el objeto r.
+            for (int iterador:recorrido){
+                r.setRuta(poi[iterador]);                //Guarda las rutas validas en el objeto r.
             }
-            r.setPuntaje(valor);                                //Guarda el valor de la ruta valida en el objeto r.
+            for (String iterador:valor) {
+                res = res.add(new BigDecimal(iterador));
+                r.setPuntaje(iterador);
+            }
+            r.setRes(res);
             lista_rutas.add(r);                                 //Guarda cada ruta valida el ArrayList lista_rutas.
         }else{
             for (int i = 0; i< poi_conectados.get(recorrido.get(recorrido.size()-1)).getConexiones().size(); i++) {              //Limite del for es la cantidad maxima de posibilidades en un POI. Se obtiene con la cantidad de posibilidades admitidas del último POI recorrido.
                 if(!recorrido.contains(poi_conectados.get(recorrido.get(recorrido.size() - 1)).getConexiones().get(i))) {      //Es verdadero si en las rutas de "recorrido" contiene las posibilidades del último POI recorrido. Esto se hace para no repetir el mismo POI. ! para ejecutar la instrucción en el caso de que no la contenga.
-                    //valor += poi_conectados.get(recorrido.get(recorrido.size() - 1)).getValor().get(i);        //Obtiene el valor de un POI con otro. Último POI del recorrido y el POI siguiente.
-                    formar_posi((ArrayList<Integer>) recorrido.clone(), poi_conectados.get(recorrido.get(recorrido.size() - 1)).getConexiones().get(i), valor); //Se envía el recorrido, la ubicación del POI siguiente en pos_admitidas y el valor sumado hasta ahora.
+                    formar_posi((ArrayList<Integer>) recorrido.clone(), (ArrayList<String>) valor.clone(), poi_conectados.get(recorrido.get(recorrido.size() - 1)).getConexiones().get(i), poi_conectados.get(recorrido.get(recorrido.size() - 1)).getValor().get(i)); //Se envía el recorrido, la ubicación del POI siguiente en pos_admitidas y el valor sumado hasta ahora.
                 }
             }
         }
 
+    }
+
+    public static void mostrar_rutas(){
+        for(Ruta iterador_rutas:lista_rutas){
+            System.out.println(iterador_rutas.getRuta()+" Valor: "+iterador_rutas.getPuntaje()+" Resultado: "+iterador_rutas.getRes());                  //Imprime las rutas validas/optimas con su valor.
+        }
+    }
+
+    public static void rutas_aptas(){
+        BigDecimal res_ant=new BigDecimal("9999");
+        ArrayList<Integer> index = new ArrayList<Integer>();
+        int cont=0;
+        for(Ruta iterador_rutas:lista_rutas){
+            if(res_ant.compareTo(iterador_rutas.getRes())==1){
+                index.clear();
+                index.add(cont);
+                res_ant=iterador_rutas.getRes();
+            }else if(res_ant.compareTo(iterador_rutas.getRes())==0){
+                index.add(cont);
+            }
+            cont++;
+        }
+
+        System.out.println("\nRutas más aptas:");
+        for (int iterador:index) {
+            System.out.println(lista_rutas.get(iterador).getRuta()+"Valor: "+lista_rutas.get(iterador).getRes());
+        }
     }
 
 }
--- src/Paquete/Ruta.java
+++ src/Paquete/Ruta.java
@@ -1,11 +1,21 @@
 package Paquete;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Ruta {
     private ArrayList<String> ruta = new ArrayList<String>();
-    private double puntaje = 0;
+    private ArrayList<BigDecimal> puntaje = new ArrayList<BigDecimal>();
+    private BigDecimal res;
+
+    public BigDecimal getRes() {
+        return res;
+    }
+
+    public void setRes(BigDecimal res) {
+        this.res = res;
+    }
 
     public ArrayList<String> getRuta() {
         return ruta;
@@ -15,11 +25,11 @@ public class Ruta {
         this.ruta.add(ruta);
     }
 
-    public double getPuntaje() {
+    public ArrayList<BigDecimal> getPuntaje() {
         return puntaje;
     }
 
-    public void setPuntaje(double puntaje) {
-        this.puntaje = puntaje;
+    public void setPuntaje(String puntaje) {
+        this.puntaje.add(new BigDecimal(puntaje));
     }
 }
